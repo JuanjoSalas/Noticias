db620d0aba42bfec909b0c0bd65b12f51def5987
juan salas <juanjo.salas.dev@gmail.com>
2024-05-25 18:50:58 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from "./components/Header/Header.jsx";
+import Home from "./components/Home/Home.jsx";
+import Form from "./components/Form/Form.jsx";
+import ListNews from "./components/ListNews/ListNews.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+
+ 
+function App() {
+
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+    <Route path="/home" element={<Home/>}/>
+      <Route path="/form" element={<Form/>}/>
+      <Route path="/list" element={<ListNews/>}/>
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Header.scss";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="Header">
+        <span>Header</span>
+        <nav>
+            <span><Link to="/home">Home</Link></span>
+            <span><Link to="/form">Add New</Link></span>
+            <span><Link to="/list">News</Link></span>
+        </nav>
+    </div>
+  )
+}
+
+export default Header;
